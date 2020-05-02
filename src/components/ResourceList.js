5a8489976dcfaa2c7b756581c4441045677fcaed
Nick Turner <recbrasil5@gmail.com>
2020-05-02 00:42:06 -0500

    324. Fetching a Given Resource

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = { resources: [] };
+
+    async componentDidMount() {
+      const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+        
+      this.setState({ resources: response.data });
+    }
+
     render() {
         return (
             <div>
-                {this.props.resource}
+                {this.state.resources.length}
             </div>
         );
     }
